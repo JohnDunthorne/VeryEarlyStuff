7b980afe0b729a6a2c3e704c6e027627030d1595
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-05 14:41:49 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+print("*#" * 20)
+print("this is an expression")
+price = 10
+print(price)
+print("this is an integer")
+rating = 4.9
+print(rating)
+print("this is a float value")
+print("John Dunthorne")
+print("this is a string")
+is_published = True
+print("is_published = True")
+print("this is a boolean")
+Patient_name = "John Smith"
+Age = 20
+is_new_patient = True
+Name = input("what is your name? ")
+print("hi " + Name)
+Fav_colour = input("So " + Name + ", What is your Favourite Colour? ")
+print("wow! I love " + Fav_colour + " Too!")
